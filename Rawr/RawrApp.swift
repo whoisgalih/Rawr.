bad5df16d48effd418557592fbd1f76d3dcfb12b
whoisgalih <galihakbar.ga91@gmail.com>
2022-09-18 19:05:25 +0700

    Initial Commit

---
+++ Rawr/RawrApp.swift
@@ -0,0 +1,17 @@
+//
+//  RawrApp.swift
+//  Rawr
+//
+//  Created by Galih Akbar on 18/09/22.
+//
+
+import SwiftUI
+
+@main
+struct RawrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
