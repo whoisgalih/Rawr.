659f6ab999200b633dfa7b72dad55a2483f33f90
Galih <galihakbar.ga91@gmail.com>
2024-10-07 09:10:15 +0700

    core data process

---
+++ Rawr/Pages/ProfileEditView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileEditView.swift
+//  Rawr
+//
+//  Created by Galih Akbar on 06/11/22.
+//
+
+import SwiftUI
+
+struct ProfileEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditView()
+    }
+}
---
+++ Rawr/Storage/UsersDefault/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  Rawr
+//
+//  Created by Galih Akbar on 04/11/22.
+//
+
+import Foundation
